9576af41b3df262405323857134a06262ff18bd7
Vimalan S <vimalansenthil@gmail.com>
2023-05-30 12:37:25 +0530

    Changed PPT

--- React-frontend/src/components/AddDoctorComponent.js
+++ React-frontend/src/components/AddDoctorComponent.js
@@ -67,7 +67,7 @@ const AddDoctorComponent = () =>{
    return(
        <div>
            <br /><br />
-           <div className = "container">
+           <div className = "container" style={{marginTop:'200px'}}>
                 <div className = "row">
                     <div className = "card col-md-6 offset-md-3 offset-md-3">
                     
